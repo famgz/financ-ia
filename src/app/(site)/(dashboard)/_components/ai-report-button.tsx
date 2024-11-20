1bcbf213a155c6b65ea81d21ef6fc75400d7051d
famgz <famgz@proton.me>
2024-11-20 18:14:23 -0300

    feat: change ai report modal based on user type of plan

--- src/app/(site)/(dashboard)/_components/ai-report-button.tsx
+++ src/app/(site)/(dashboard)/_components/ai-report-button.tsx
@@ -13,14 +13,10 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+
 import { DUMMY_REPORT } from "@/constants/report";
 import { BotIcon, Loader2Icon } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 import Markdown from "react-markdown";
 
@@ -46,58 +42,63 @@ export default function AIReportButton({ month, userHasPremiumPlan }: Props) {
   }
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div>
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className="rounded-full"
-                  disabled={!userHasPremiumPlan}
-                >
-                  Relatório IA
-                  <BotIcon />
-                </Button>
-              </AlertDialogTrigger>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"outline"} className="rounded-full">
+          Relatório IA
+          <BotIcon />
+        </Button>
+      </AlertDialogTrigger>
 
-              <AlertDialogContent className="max-w-[600px]">
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Relatório IA</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Use inteligência artificial para gerar um relatório com
-                    insights sobre suas finanças.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <ScrollArea className="prose max-h-[450px] text-muted-foreground prose-h1:text-foreground prose-h3:text-foreground prose-h4:text-foreground prose-strong:text-foreground">
-                  <Markdown>{report}</Markdown>
-                </ScrollArea>
-                <AlertDialogFooter>
-                  <AlertDialogCancel asChild>
-                    <Button variant={"ghost"}>Cancelar</Button>
-                  </AlertDialogCancel>
-                  <Button
-                    onClick={handleGenerateReportClick}
-                    disabled={loading}
-                    className="gap-2"
-                  >
-                    {loading && <Loader2Icon className="animate-spin" />}
-                    Gerar Relatório
-                  </Button>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        </TooltipTrigger>
-
-        {!userHasPremiumPlan && (
-          <TooltipContent>
-            <p>Esta funcionalidade é apenas disponível para membros Premium.</p>
-            <p>Atualize seu plano para ter acesso.</p>
-          </TooltipContent>
-        )}
-      </Tooltip>
-    </TooltipProvider>
+      {userHasPremiumPlan ? (
+        <>
+          {" "}
+          <AlertDialogContent className="max-w-[600px]">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Relatório IA</AlertDialogTitle>
+              <AlertDialogDescription>
+                Use inteligência artificial para gerar um relatório com insights
+                sobre suas finanças.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <ScrollArea className="prose max-h-[450px] text-muted-foreground prose-h1:text-foreground prose-h3:text-foreground prose-h4:text-foreground prose-strong:text-foreground">
+              <Markdown>{report}</Markdown>
+            </ScrollArea>
+            <AlertDialogFooter>
+              <AlertDialogCancel asChild>
+                <Button variant={"ghost"}>Cancelar</Button>
+              </AlertDialogCancel>
+              <Button
+                onClick={handleGenerateReportClick}
+                disabled={loading}
+                className="gap-2"
+              >
+                {loading && <Loader2Icon className="animate-spin" />}
+                Gerar Relatório
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </>
+      ) : (
+        <>
+          <AlertDialogContent className="max-w-[600px]">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Relatório IA</AlertDialogTitle>
+              <AlertDialogDescription>
+                Você precisa de um plano premium para gerar relatórios com IA.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter className="mt-10">
+              <AlertDialogCancel asChild>
+                <Button variant={"ghost"}>Cancelar</Button>
+              </AlertDialogCancel>
+              <Button onClick={handleGenerateReportClick} className="gap-2">
+                <Link href={"/subscription"}>Assinar plano premium</Link>
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </>
+      )}
+    </AlertDialog>
   );
 }

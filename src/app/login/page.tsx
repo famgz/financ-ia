160ce032126150d1bfff97a5c5b34880554c2dea
famgz <famgz@proton.me>
2024-11-20 13:21:41 -0300

    feat: add login page responsivity

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,14 +14,14 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="grid size-full grid-cols-2">
-      <div className="mx-auto flex h-full max-w-[600px] flex-col justify-center p-8">
+    <div className="flex size-full flex-col max-sm:justify-center sm:grid sm:grid-cols-2">
+      <div className="max-sm:bbackdrop-blur-sm z-10 mx-auto flex h-full max-w-[600px] flex-col justify-center from-background/0 via-background to-background/0 p-8 max-sm:bg-gradient-to-b">
         <LogoWithName />
         <h1 className="mt-8 text-4xl font-bold">Bem-vindo</h1>
         <p className="mt-3 text-muted-foreground">
-          A Financ.IA é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+          A Financ.IA é uma plataforma de gestão financeira que utiliza
+          Inteligência Artificial para monitorar suas movimentações, e oferecer
+          insights personalizados, facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
           <Button variant={"outline"} className="mt-8 gap-2">
@@ -31,12 +31,12 @@ export default async function LoginPage() {
         </SignInButton>
       </div>
 
-      <div className="relative size-full">
+      <div className="size-full max-sm:absolute sm:relative">
         <Image
           src={"/login-hero.jpg"}
           alt="Imagem Hero de Login"
           fill
-          className="object-cover"
+          className="z-0 object-cover"
         />
       </div>
     </div>
